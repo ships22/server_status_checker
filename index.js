5e5957f478bd057091d3f5d11cbdd441e3a6bdc1
ships22 <sshipsss@gmail.com>
2020-11-28 07:55:10 +0100

    index js modif

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
+//dependencies -
 const http = require('http')
 
-// defining serverside app scaffholder
+// app object module scaffholding -
 const app = {}
+
+//app configuration -
+app.config = {
+  PORT: 3001,
+}
+
+//creating server -
+app.createServer = () => {
+  const server = http.createServer(app.handleReqRes)
+  server.listen(app.config.PORT, () => {
+    console.log(`Server is listnening on port ${app.config.PORT}`)
+  })
+}
+
+//handling request and response -
+app.handleReqRes = (request, respone) => {
+  respone.end('tested ok!!!...')
+}
+
+//starting server -
+app.createServer()
